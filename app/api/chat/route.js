ed0832da3aba85501ae915e5c7e0edf5dbd0fc17
Ammiel Bowen <ab22dv@fsu.edu>
2024-08-11 13:50:22 -0400

    Creation of a basic chatbot

---
+++ app/api/chat/route.js
@@ -0,0 +1,68 @@
+import {NextResponse} from 'next/server'
+import OpenAI from 'openai'
+
+const systemPrompt = 'You are an AI-powered customer support assistant for NeuroAI, a platform dedicated to providing AI-driven support for students interested in the field of computational neuroscience field.'
+/*
+Embedding is what the user puts in'
+1 hour: 45 minutes
+must be implemented in javascript
+most of code is same
+js version of some code
+to insert into pinecone script allows, but take the query and convert into bedding.
+show response to user in front end.
+look at docs for pinecone and langchain, same idea. just syntax
+langchain pinecone website shows basically exact code.
+https://js.langchain.com/v0.2/docs/integrations/vectorstores/pinecone/
+
+/*
+
+/*
+1. NeuroAI offers AI-powered interviews for neuroscience students.
+2. Our platform helps candidates practice and prepare for real neuroscience-related interviews.
+3. We cover a wide range of topics, including cognitive science, neuroimaging, and computational neuroscience.
+4. Users can access our services through our website or mobile app.
+5. If asked about technical issues, guide users to our troubleshooting page or suggest contacting our technical support team.
+6. Always maintain user privacy and do not share personal information.
+7. If you're unsure about any information, it's okay to say you don't know and offer to connect the user with a human representative.
+
+Your goal is to privde accurate information, assist with common inquiries, and ensure a positive experience for all NeuroAI users.
+*/
+
+export async function POST(req){    //frontend
+    const openai = new OpenAI()
+    const data = await req.json()
+
+    const completion = await openai.chat.completions.create({   //completion
+        messages: [
+            {
+                role: 'system', 
+                content: systemPrompt
+            },
+            ...data,
+        ],
+        model: 'gpt-4o-mini',
+        stream: true,
+    })
+
+    const stream = new ReadableStream({ //start streaming it
+        async start(controller){
+            const encoder = new TextEncoder()
+            try{
+                for await (const chunk of completion){
+                    const content = chunk.choices[0]?.delta?.content
+                    if (content){
+                        const text = encoder.encode(content)
+                        controller.enqueue(text)
+                    }
+                }
+            }
+            catch(error){
+                controller.error(err)
+            } finally {
+                controller.close()
+            }
+        },
+    })
+
+    return new NextResponse(stream) //return stream
+}
